b24b2f96a6ab0440dbbcfbfd42a64606da5cb1b6
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-07-19 12:56:15 +0530

    search rotated 2

--- 12 BinarySearch/searchrotated2.cpp
+++ 12 BinarySearch/searchrotated2.cpp
@@ -1,13 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-int searchRotated(int a[] ,int n,int target){
+//duplicates allowed
+
+bool searchRotated(int a[] ,int n,int target){
     int low = 0 , high = n-1 ;
 
     while(low <= high){          // tc : o(logn)
         int mid = (low+high)/2 ;
-        if(a[mid] == target) return mid ;
+        if(a[mid] == target) return true ;
         //left sorted
+        if(a[low] == a[mid] == a[high]){
+            low++;
+            high-- ;
+            continue;
+        }
         if ( a[low] <= a[mid]){
             if(target <= a[mid] && target >= a[low]){
                 high = mid-1 ;
@@ -25,13 +32,13 @@ int searchRotated(int a[] ,int n,int target){
             }
         }
     }
-    return -1 ;
+    return false ;
     
 }
 
 int main(){
     int n =9 ;
-    int a[n] = {7,8,9,1,2,3,4,5,6} ;
+    int a[n] = {6,6,7,1,2,3,4,5,6} ;
 
     int x ;
     cin >> x ;
