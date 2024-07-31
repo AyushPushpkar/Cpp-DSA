c5fe52c1ccca894887bd812ce0498277d864df6f
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-07-31 14:53:07 +0530

    optimal median

--- 14 BinarySearch3/mediansortedarr.cpp
+++ 14 BinarySearch3/mediansortedarr.cpp
@@ -1,8 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
+double optimal(int arr1[],int n ,int arr2[] , int m){
+    // for arr1 is always smaller
+    if(n>m) return optimal(arr2,m,arr1,n) ;
+    int low = 0 , high = n ;      // tc : o(min(logn , logm))
+    int left = (n+m+1)/2 ;
+    int total = n+m ;
+
+    while(low <=high){
+        int mid1 = (low+high) >> 1 ; // right shift
+        int mid2 = left - mid1 ;
+        int l1 = INT_MIN , l2 = INT_MIN ;
+        int r1 = INT_MAX , r2 = INT_MAX ;
+        if(mid1 < n) r1 = arr1[mid1] ;
+        if(mid2 < m) r2 = arr2[mid2] ;
+        if(mid1 - 1 >= 0) l1 = arr1[mid1 -1] ;
+        if(mid2 -1 >= 0) l2 = arr2[mid2-1] ;
+
+        if(l1 <= r2 && l2 <= r1){
+            if(total%2==1) return max(l1,l2) ;
+            return (double)(max(l1,l2) + min(r1,r2))/2.0 ;
+        }
+        else if(l1 > r2) high = mid1-1 ;
+        else low = mid1+1 ;
+    }
+    return -1 ;
+}
+
 double median(int arr1[],int n ,int arr2[] , int m){
-    vector<int>v; //temp container
     int left = 0;
     int right = 0 ;
     int total = n+m ;
@@ -10,7 +36,7 @@ double median(int arr1[],int n ,int arr2[] , int m){
     int ind1 = ind2 -1 ;
     int cnt =0 ;
     int el1 = -1 , el2 = -1 ;
-    while((left<n)&&(right<m)){
+    while((left<n)&&(right<m)){         // o(total)   TLE
         if(arr1[left]<=arr2[right]) {
             if(cnt == ind1) el1 = arr1[left] ;
             if(cnt == ind2) el2 = arr1[left] ;
@@ -47,10 +73,10 @@ double median(int arr1[],int n ,int arr2[] , int m){
 }
 
 double merge(int arr1[],int n ,int arr2[] , int m){
-    vector<int>v; //temp container
+    vector<int>v;   // sc : o(total)
     int left = 0;
     int right = 0 ;
-    while((left<n)&&(right<m)){
+    while((left<n)&&(right<m)){          //o (total)
         if(arr1[left]<=arr2[right]) v.push_back(arr1[left ++]);
         else v.push_back(arr2[right++]) ;
     }
@@ -81,4 +107,6 @@ int main(){
     cout << merge(arr1,n,arr2,m) << endl ;
 
     cout << median(arr1,n,arr2,m) << endl ;
+
+    cout << optimal(arr1,n,arr2,m) << endl ;
 }
