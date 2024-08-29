03f5411c82618bb00192fdf1eea55d4110bdaffd
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-08-29 23:10:44 +0530

    deserialize

--- 17 BinaryTree2/serialize.cpp
+++ 17 BinaryTree2/serialize.cpp
@@ -36,6 +36,66 @@ string serialize(Node* root ){
 
 }
 
+Node* deserialize(string data){
+    if(data.size() == 0) return nullptr ;
+
+    stringstream s(data) ;
+    string str ;
+    getline(s,str ,',') ;
+    Node *root = new Node(stoi(str)) ;
+    queue<Node*> q ; 
+    q.push(root) ;
+
+    while(!q.empty()){
+
+        Node* node = q.front() ;
+        q.pop() ;
+
+        getline(s,str,',') ;
+        if(str == "#"){
+            node ->left == nullptr ;
+        }
+        else{
+            Node* leftNode = new Node(stoi(str)) ;
+            node->left = leftNode ; 
+            q.push(leftNode) ;
+        }
+
+        getline(s,str,',') ;
+        if(str == "#"){
+            node ->right == nullptr ;
+        }
+        else{
+            Node* rightNode = new Node(stoi(str)) ;
+            node->right = rightNode ; 
+            q.push(rightNode) ;
+        }
+        
+    }
+    return root ;
+}
+
+void printLevelOrder(Node* root) {
+    if (!root) return;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size(); 
+        for (int i = 0; i < levelSize; ++i) {
+            Node* node = q.front();
+            q.pop();
+
+            cout << node->data << " ";
+
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+        cout << endl; 
+    }
+}
+
 int main(){
 
     struct Node* root = new Node(1);
@@ -46,11 +106,13 @@ int main(){
     root -> left -> right = new Node(5) ;
     root -> right -> left = new Node(6) ;
     root -> right -> right = new Node(7) ;
-    root -> left -> left ->left = new Node(8) ;
-    root -> left -> left ->right = new Node(9) ;
+    root -> left -> right ->left = new Node(8) ;
+    root -> left -> right ->right = new Node(9) ;
 
-    Node* node = root -> left -> right ;
+    string res = serialize(root) ;
+    cout << res << endl ;
+    cout << endl ;
 
-    cout << serialize(root) << endl ;
-
+    Node* ansRoot = deserialize(res) ;
+    printLevelOrder(ansRoot) ;
 }
