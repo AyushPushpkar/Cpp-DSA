a16f2ad7166faa3659c81b6eae61802bc7cd0177
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-11-22 19:28:46 +0530

    balance paranthesis

---
+++ 20 StackQueue/balancebracket.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+bool balancedParanthesis(string s ){  // tc : o(n)   // sc : o(n)
+    int len = s.size() ;
+    stack<int> st ;
+
+    for(int i =0 ; i<len ;i++){
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[' ){
+            st.push(s[i]) ;
+        }
+        else{
+            if(st.empty()) return false ;
+            char ch = st.top() ;
+            st.pop() ;
+            if((s[i] == ')' && ch == '(') || (s[i] == ']' && ch == '[')
+                    || (s[i] == '}' && ch == '{') ){   
+            }
+            else{
+                return false ;
+            }
+        }
+    }
+
+    return st.empty() ;
+
+}
+
+int main(){
+    string s = "({}[(){{}]})" ;
+    cout << balancedParanthesis(s) ;
+}
