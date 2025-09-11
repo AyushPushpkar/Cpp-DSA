60c12bb2cee94465bf9595f0b0dd76da61d614e2
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-09-12 00:58:43 +0530

    kosaraju algo

---
+++ 27 Graph5/kosarajualgo.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//?strongly connected components
+//* tc : o(V+E)
+
+void dfs(vector<vector<int>> &adj, vector<int> &vis, int startNode, stack<int> &finishOrder)
+{
+
+    vis[startNode] = 1;
+
+    for (auto it : adj[startNode])
+    {
+        int adjNode = it;
+        if (!vis[adjNode])
+        {
+            dfs(adj, vis, adjNode, finishOrder);
+        }
+    }
+
+    finishOrder.push(startNode);
+}
+
+void dfs2(vector<vector<int>> &adj, vector<int> &vis, int startNode)
+{
+
+    vis[startNode] = 1;
+
+    for (auto it : adj[startNode])
+    {
+        int adjNode = it;
+        if (!vis[adjNode])
+        {
+            dfs2(adj, vis, adjNode);
+        }
+    }
+}
+
+int kosaraju(int V, vector<vector<int>> &adj)
+{
+
+    vector<int> visited(V, 0);
+    stack<int> finishOrder;
+
+    for (int i = 0; i < V; i++)
+    {
+        if (!visited[i])
+            dfs(adj, visited, i, finishOrder);
+    }
+
+    vector<vector<int>> newAdj;
+
+    for (int i = 0; i < V; i++)
+    {
+        visited[i] = 0;
+        for (auto it : adj[i])
+        {
+            newAdj[it].push_back(i);
+        }
+    }
+
+    int sccs = 0;
+    while (!finishOrder.empty())
+    {
+        int node = finishOrder.top();
+        finishOrder.pop();
+
+        if (!visited[node])
+        {
+            sccs++;
+            dfs2(newAdj, visited, node);
+        }
+    }
+
+    return sccs;
+}
